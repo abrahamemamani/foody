a8da9b2760ba869e40a3013ef3209190a295aaad
abrahamemamani <abraham.e.mamani@gmail.com>
2018-08-22 14:33:33 -0300

    Funcionamiento al menú + últimos arreglos estéticos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 $("[data-component-name~='page-to-page']").initTo();
+
+/*##
+  # Funcion "goto" (al clickear)
+  ## */
+$('[data-goto-id]').on('click', function(e){
+    e.preventDefault();
+    goto($(this).attr('data-goto-id'));
+});
+
+function goto(link){
+    let pos = $(link).offset().top;
+    $('html, body').animate({
+        scrollTop: pos
+    }, 1000);
+}
